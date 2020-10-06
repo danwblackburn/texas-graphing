7b01a8206646b9e016cd8a3211b8eae9cc0f87e4
danwblackburn <danwb@stanford.edu>
2020-10-07 00:31:35 +0200

    Changed animation labeling

--- animated_graph.py
+++ animated_graph.py
@@ -2,32 +2,29 @@ import matplotlib.animation as ani
 import matplotlib.pyplot as plt
 import matplotlib.dates as dates
 import numpy as np
-import csv
+from datetime import datetime
 
 
 
 new_cases = list(np.loadtxt('texas.csv', delimiter=',', dtype = int, skiprows=1,usecols=(4)))
-date_list = list(np.loadtxt('texas.csv', delimiter=',', dtype = str, skiprows=1,usecols=(0)))
+unformatted_dates = list(np.loadtxt('texas.csv', delimiter=',', dtype = str, skiprows=1,usecols=(0)))
+date_list = [datetime.strptime(date,"%m/%d/%y").strftime('%d, %b') for date in unformatted_dates]
 
 date_axis = [dates.datestr2num(date) for date in date_list]
 
 fig = plt.figure() 
-plt.xticks(rotation=45, ha="right", rotation_mode="anchor")
-plt.subplots_adjust(bottom = 0.2, top = 0.9)
-plt.ylabel('No of Deaths')
-plt.xlabel('Dates')
+plt.ylabel('New Cases')
+plt.title('Texas Corona Cases')
+plt.xticks([])
 ax = plt.gca()
-ax.xaxis.set_major_formatter(dates.DateFormatter('%Y-%m-%d'))
-line, = ax.plot([], [], lw=2) 
+ax.xaxis.set_major_formatter(dates.DateFormatter('%m, %b'))
+line, = ax.plot([], [], lw=1) 
 
 def animate(i):
-    if(i < 50):
-        line.set_data(date_axis[:i], new_cases[:i])
-    else:
-        line.set_data(date_axis[i - 50:i], new_cases[i - 50:i])
-    ax.relim()
-    ax.autoscale_view()
+    line.set_data(date_axis[:i], new_cases[:i])
+    ax.set_xlim(date_axis[0], date_axis[i - 1] + 5)
     ax.set_ylim(0, max(new_cases))
+    plt.xlabel(date_list[i - 1])
 
 anim = ani.FuncAnimation(fig, animate, interval=40, frames=len(new_cases) + 1)
 
