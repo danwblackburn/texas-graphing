69d4b58f5757c2abe33c2fd470f614cd6891da3c
danwblackburn <danwb@stanford.edu>
2020-10-07 22:15:29 +0200

    Made it more pretty

--- animated_graph.py
+++ animated_graph.py
@@ -4,29 +4,43 @@ import matplotlib.dates as dates
 import numpy as np
 from datetime import datetime
 
+title_font = {'family': 'helvetica',
+        'color':  'darkred',
+        'weight': 'normal',
+        'size': 22,
+        }
 
+ylabel_font = {'family': 'helvetica',
+        'color':  'black',
+        'weight': 'normal',
+        'size': 16,
+        }
+
+xlabel_font = {'family': 'helvetica',
+        'color':  'black',
+        'weight': 'normal',
+        'size': 16,
+        }
 
 new_cases = list(np.loadtxt('texas.csv', delimiter=',', dtype = int, skiprows=1,usecols=(4)))
 unformatted_dates = list(np.loadtxt('texas.csv', delimiter=',', dtype = str, skiprows=1,usecols=(0)))
-date_list = [datetime.strptime(date,"%m/%d/%y").strftime('%d, %b') for date in unformatted_dates]
+date_list = [datetime.strptime(date,"%m/%d/%y").strftime('%d %b') for date in unformatted_dates]
 
 date_axis = [dates.datestr2num(date) for date in date_list]
 
-fig = plt.figure() 
-plt.ylabel('New Cases')
-
-plt.title('Texas Corona Cases')
+fig = plt.figure(figsize=(10, 7.5)) 
+plt.ylabel('New Cases', fontdict=ylabel_font)
+plt.title('Texas Corona Cases', fontdict=title_font)
 plt.xticks([])
 ax = plt.gca()
-ax.xaxis.set_major_formatter(dates.DateFormatter('%m, %b'))
-line, = ax.plot([], [], lw=1) 
+line, = ax.plot([], [], color='red', lw=2) 
 
 def animate(i):
     line.set_data(date_axis[:i], new_cases[:i])
     ax.set_xlim(date_axis[0], date_axis[i - 1] + 5)
-    ax.set_ylim(0, max(new_cases))
+    ax.set_ylim(0, max(new_cases) + 1000)
     if not i == 0:
-        plt.xlabel(date_list[i - 1])
+        plt.xlabel(date_list[i - 1], fontdict=xlabel_font, labelpad=10)
 
 anim = ani.FuncAnimation(fig, animate, interval=97, frames=len(new_cases) + 1)
 
